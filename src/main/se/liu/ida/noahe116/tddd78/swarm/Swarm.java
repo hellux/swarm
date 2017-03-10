a7e6a3670d395921b1d326a39cb1744ea089d155
Hellux <hellux07@gmail.com>
2017-03-11 00:10:21 +0100

    added window creation for Swarm project

---
+++ src/main/se/liu/ida/noahe116/tddd78/swarm/Swarm.java
@@ -0,0 +1,13 @@
+package se.liu.ida.noahe116.tddd78.swarm;
+
+import javax.swing.JFrame;
+
+public class Swarm {
+    private static final String WINDOW_TITLE = "Swarm";
+
+    public static void main(String args[]) {
+        JFrame frame = new JFrame(WINDOW_TITLE);
+
+        frame.setVisible(true);
+    }
+}        
