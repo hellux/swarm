624f496ddb04af09c88ea0edd30c55a41156e6d4
Hellux <hellux07@gmail.com>
2017-03-11 17:42:20 +0100

    added GamePanel to panel

---
+++ src/main/se/liu/ida/noahe116/tddd78/swarm/ui/MainPanel.java
@@ -0,0 +1,9 @@
+package se.liu.ida.noahe116.tddd78.swarm.ui;
+
+import javax.swing.JPanel;
+
+public class MainPanel extends JPanel {
+    public MainPanel() {
+        this.add(new GamePanel());
+    }
+}
