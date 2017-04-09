497d82f0c7683d11c77c580bcf1197a0c7aedbb3
Hellux <hellux07@gmail.com>
2017-04-09 21:28:30 +0200

    added knockback and damage on collision

--- src/se/liu/ida/noahe116/tddd78/swarm/game/components/CollisionComponent.java
+++ src/se/liu/ida/noahe116/tddd78/swarm/game/components/CollisionComponent.java
@@ -3,44 +3,88 @@ package se.liu.ida.noahe116.tddd78.swarm.game.components;
 import se.liu.ida.noahe116.tddd78.swarm.common.Vector2D;
 
 public class CollisionComponent extends Component {
-    private CollisionCircle[] collisionCircles;
+    private static double KNOCKBACK_MULTIPLIER = 0.5;
 
-    public class CollisionCircle {
-        Vector2D position;
-        public int radius;
+    public final double radius;
+    private int damage = 30;
+    private boolean knockback = false;
 
-        public CollisionCircle(int x, int y, int r) {
-            this.position.x = x;
-            this.position.y = y;
-            this.radius = r;
-        }
+    public CollisionComponent(double r) {
+        this.radius = r;
+    }
 
-        public Vector2D center() {
-            return Vector2D.add(
-                CollisionComponent.this.entity.get(PositionComponent.class).getPosition(),
-                this.position
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public void setKnockback(boolean b) {
+        this.knockback = b;
+    }
+
+    /**
+     * Check if component collides with other collision component.
+     * @param cc other collision component
+     * @return vector I of collision intersection pointing away from this
+     *
+     * Diagram:
+     *
+     *          , - ~ - ,
+     *      , '           ' ,
+     *    ,                   ,
+     *   ,                     ,
+     *  ,     this.center()     ,
+     *  ,           o           ,
+     *  ,            , - ~ - ,  ,
+     *   ,       , '  \        ; ,
+     *    ,    ,       \ I    ,    ,
+     *      , ,         \  , '      ,
+     *       ,' - , _ ,  v           ,
+     *       ,           o           ,
+     *       ,      cc.center()      ,
+     *        ,                     ,
+     *         ,                   ,
+     *           ,              , '
+     *             ' - , _ ,  '
+     *
+     *
+     * D = (cc.center() - this.center())
+     *
+     *     ^       _
+     * I = D * abs(D-this.radius-cc.radius)
+     **/
+    public Vector2D collidesWith(CollisionComponent cc) {
+        if (Vector2D.distanceSq(this.center(), cc.center()) <
+            Math.pow(this.radius+cc.radius, 2)) {
+            Vector2D difference = Vector2D.subtract(this.center(), cc.center());
+            return Vector2D.multiply(
+                difference.normal(),
+                Math.abs(difference.magnitude() - this.radius - cc.radius)
             );
         }
+        return null;
     }
 
-    public CollisionComponent(CollisionCircle...circles) {
-        this.collisionCircles = circles;
-    }
-
-    public boolean collidesWith(CollisionComponent cc) {
-        for (CollisionCircle c1 : this.collisionCircles) {
-            Vector2D c1Center = c1.center();
-            for (CollisionCircle c2 : cc.getCollisionCircles()) {
-                if (Vector2D.distanceSq(c1Center, c2.center()) <
-                    Math.pow(c1.radius+c2.radius, 2)) {
-                    return true;
-                }
+    public void collideWith(CollisionComponent cc, Vector2D intersection) {
+        HealthComponent hc = this.entity.get(HealthComponent.class);
+        if (hc != null) hc.hurt(cc.getDamage());
+        if (cc.hasKnockback()) {
+            PositionComponent pc = this.entity.get(PositionComponent.class);
+            if (pc != null) {
+                //pc.move(intersection);
+                pc.accelerate(Vector2D.multiply(intersection, KNOCKBACK_MULTIPLIER));
             }
         }
-        return false;
     }
 
-    public CollisionCircle[] getCollisionCircles() {
-        return this.collisionCircles;
+    public Vector2D center() {
+        return this.entity.get(PositionComponent.class).getPosition();
+    }
+
+    public int getDamage() {
+        return this.damage;
+    }
+
+    public boolean hasKnockback() {
+        return this.knockback;
     }
 }
